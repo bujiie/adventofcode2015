f4513f36f348313d2f40e46b3e52ee4104f440eb
brian.ujiie <brian.ujiie@workday.com>
2021-12-01 18:24:15 -0800

    Add bootstrap code.

---
+++ __template.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+
+with open(argv[1]) as fp:
+	for i, l in enumerate(fp):
+		l=l.strip()
+		print(l)
---
+++ new.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+day=$1
+
+challenge_dir="day${1}"
+
+mkdir $challenge_dir
+./get_input.py $day
+mv "in" "${challenge_dir}/in"
+
+cp __template.py "${challenge_dir}/a.py"
+cd $challenge_dir
+chmod 755 a.py
+
