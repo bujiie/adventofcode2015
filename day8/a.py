6d34106d425a10c210b7812b78b8e91211ce1226
brian.ujiie <brian.ujiie@workday.com>
2021-12-06 10:24:48 -0800

    Add solution for day8.

---
+++ day8/a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from sys import argv
+import re
+
+lc=0
+mc=0
+
+regex=r'(\\\\|\\x[A-Za-z0-9]{2}|\\")'
+
+with open(argv[1]) as fp:
+    for i, l in enumerate(fp):
+        l=l.strip()
+        lc+=len(l)
+
+        c=len(l)-2
+        results=re.findall(regex, l[1:-1])
+        if results is not None:
+            for result in results:
+                if result in ["\\\\", "\\\""]:
+                    c-=2
+                else:
+                    c-=4
+                c+=1
+        mc+=c
+print(lc-mc)
